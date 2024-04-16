8d2def199a2b43f889898f983c2c4d83039315bf
Matheus Muccio <matheus.gmuccio@gmail.com>
2024-04-16 14:19:24 -0300

    Iniciando Projeto

---
+++ src/test/java/com/muccio/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muccio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
