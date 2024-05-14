7d78d32071289f1e521b8ca34b282c74a29fefe8
Matheus Muccio <matheus.gmuccio@gmail.com>
2024-05-14 14:12:03 -0300

    feat: find all products

--- src/main/java/com/muccio/controllers/ProductController.java
+++ src/main/java/com/muccio/controllers/ProductController.java
@@ -2,12 +2,12 @@ package com.muccio.controllers;
 
 import com.muccio.models.dto.ProductDTO;
 import com.muccio.services.ProductService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -19,6 +19,12 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAll(@RequestParam(name = "name", defaultValue = "") String name, Pageable pageable) {
+        Page<ProductDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
 
--- src/main/java/com/muccio/services/ProductService.java
+++ src/main/java/com/muccio/services/ProductService.java
@@ -6,7 +6,10 @@ import com.muccio.models.entities.Product;
 import com.muccio.repositories.ProductRepository;
 import com.muccio.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
@@ -14,9 +17,16 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
         Product product =  productRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Resource Not Found."));
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(String name, Pageable pageable) {
+        Page<Product> result =  productRepository.searchByName(name, pageable);
+        return result.map(ProductDTO::new);
     }
 }
