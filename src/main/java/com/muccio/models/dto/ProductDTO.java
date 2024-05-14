9405042e5dba78feb9085fbef38df8ba1ba9feb8
Matheus Muccio <matheus.gmuccio@gmail.com>
2024-05-14 13:14:50 -0300

    feat: apply jakarta validators in productDTO

--- src/main/java/com/muccio/models/dto/ProductDTO.java
+++ src/main/java/com/muccio/models/dto/ProductDTO.java
@@ -1,17 +1,21 @@
 package com.muccio.models.dto;
 
 import com.muccio.models.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
-
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres.")
+    @NotBlank(message = "Campo requerido.")
     private String name;
-
+    @Size(min = 3, message = "Descrição precisa ter no mínimo 10 caracteres.")
+    @NotBlank(message = "Campo requerido.")
     private String description;
-
+    @Positive(message = "O preço deve ser positivo.")
     private Double price;
-
     private String imgUrl;
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
