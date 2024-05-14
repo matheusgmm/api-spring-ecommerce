49ce32d26b1ef8ac54d4c2d87bb75d0430f54160
Matheus Muccio <matheus.gmuccio@gmail.com>
2024-05-14 13:37:42 -0300

    feat: create custom exceptions handlers

---
+++ src/main/java/com/muccio/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.muccio.services.exceptions;
+
+public class ResourceNotFoundException  extends RuntimeException {
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
