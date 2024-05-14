b7f3a7d03b54c64e8f553f80d108d84b5e083f34
Matheus Muccio <matheus.gmuccio@gmail.com>
2024-05-14 14:02:31 -0300

    feat: findById and import SQL

---
+++ src/main/java/com/muccio/controllers/ProductController.java
@@ -0,0 +1,25 @@
+package com.muccio.controllers;
+
+import com.muccio.models.dto.ProductDTO;
+import com.muccio.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+}
--- src/main/java/com/muccio/services/ProductService.java
+++ src/main/java/com/muccio/services/ProductService.java
@@ -1,7 +1,10 @@
 package com.muccio.services;
 
 
+import com.muccio.models.dto.ProductDTO;
+import com.muccio.models.entities.Product;
 import com.muccio.repositories.ProductRepository;
+import com.muccio.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,5 +14,9 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-
+    public ProductDTO findById(Long id) {
+        Product product =  productRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Resource Not Found."));
+        return new ProductDTO(product);
+    }
 }
