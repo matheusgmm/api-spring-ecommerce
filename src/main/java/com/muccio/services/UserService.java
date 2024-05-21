07da542ef263c08509fc274180fd3bc64efb23fd
Matheus Muccio <matheus.gmuccio@gmail.com>
2024-05-21 14:03:32 -0300

    feat: Implements UserDetailsService

---
+++ src/main/java/com/muccio/services/UserService.java
@@ -0,0 +1,36 @@
+package com.muccio.services;
+
+import com.muccio.models.entities.Role;
+import com.muccio.models.entities.User;
+import com.muccio.projections.UserDetailsProjection;
+import com.muccio.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.isEmpty()) {
+            throw new UsernameNotFoundException("Email not found.");
+        }
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+        return user;
+    }
+
+}
