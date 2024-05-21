7aa889fa50236c375409bae258eecd6b9e452efa
Matheus Muccio <matheus.gmuccio@gmail.com>
2024-05-21 15:50:28 -0300

    fix: validation

--- src/main/java/com/muccio/models/dto/ProductDTO.java
+++ src/main/java/com/muccio/models/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.muccio.models.dto;
 
 import com.muccio.models.entities.Category;
 import com.muccio.models.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +16,7 @@ public class ProductDTO {
     @Size(min = 3, message = "Descrição precisa ter no mínimo 10 caracteres.")
     @NotBlank(message = "Campo requerido.")
     private String description;
+    @NotNull(message = "Campo requerido.")
     @Positive(message = "O preço deve ser positivo.")
     private Double price;
     private String imgUrl;
