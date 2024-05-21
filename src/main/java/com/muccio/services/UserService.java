de2c95d0bfda09a44e831077992866b1dbbaa8c6
Matheus Muccio <matheus.gmuccio@gmail.com>
2024-05-21 14:43:25 -0300

    feat: GET User Logged

--- src/main/java/com/muccio/services/UserService.java
+++ src/main/java/com/muccio/services/UserService.java
@@ -1,16 +1,22 @@
 package com.muccio.services;
 
+import com.muccio.models.dto.UserDTO;
 import com.muccio.models.entities.Role;
 import com.muccio.models.entities.User;
 import com.muccio.projections.UserDetailsProjection;
 import com.muccio.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -31,6 +37,25 @@ public class UserService implements UserDetailsService {
             user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
         }
         return user;
+    }
+
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+
+            return repository.findByEmail(username).orElseThrow(() -> new NoSuchElementException("User not found."));
+        }
+        catch (Exception e) {
+            throw new UsernameNotFoundException("Email not found.");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe() {
+        User user = authenticated();
+        return new UserDTO(user);
     }
 
 }
