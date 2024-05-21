e25a47a102e5a8b651a0d254b793f0efb2404efb
Matheus Muccio <matheus.gmuccio@gmail.com>
2024-05-21 16:27:05 -0300

    wip: configurating OpenAPI Swagger

--- src/main/java/com/muccio/controllers/UserController.java
+++ src/main/java/com/muccio/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.muccio.controllers;
 
 import com.muccio.models.dto.UserDTO;
 import com.muccio.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication Endpoint")
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
@@ -16,6 +19,7 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @Operation(summary = "Return information's about logged user")
     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_CLIENT')")
     @GetMapping(value = "/me")
     public ResponseEntity<UserDTO> getMe() {
