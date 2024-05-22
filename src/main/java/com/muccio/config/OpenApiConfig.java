ea92f0a07bf1aaa51d2d6d2c1bb200f5ecfd26d2
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-05-22 09:04:52 -0300

    fix: update swagger configs and description

--- src/main/java/com/muccio/config/OpenApiConfig.java
+++ src/main/java/com/muccio/config/OpenApiConfig.java
@@ -11,17 +11,17 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenApi() {
         return new OpenAPI()
-                .info(
-                        new Info()
-                                .title("API ECommerce")
-                                .version("v1")
-                                .description("Some description about your API.")
-                                .termsOfService("http://terms-of-service.com.br")
-                                .license(
-                                        new License().name("Apache 2.0")
-                                                .url("http://license-url.com.br")
-                                )
-                );
+            .info(
+                new Info()
+                    .title("API ECommerce")
+                    .version("v1")
+                    .description("API for managing an e-commerce platform, offering endpoints for operations with categories, products, orders, users, and other essential functionalities for running an online store. This API allows integration with frontend systems and mobile applications, providing a robust and secure interface for the administration and operation of a virtual store.")
+                    .termsOfService("http://terms-of-service.com.br")
+                    .license(
+                        new License().name("Apache 2.0")
+                            .url("http://license-url.com.br")
+                    )
+            );
 
     }
 }
